e396221b203e193ba27b02011bd1471675f8eacf
Azharul-Raj <azharulislam.raj512@gmail.com>
2022-09-02 08:37:46 +0600

    card added into the container

--- js/app.js
+++ js/app.js
@@ -34,6 +34,24 @@ const callProducts = async () => {
 // display product in the feed
 const displayProducts = (products) => {
     const productsContainer = document.getElementById('products-container')
+    products.forEach(product => {
+        const { title, price, image, rating } = product
+        const { rate, count } = rating
+        const div = document.createElement('div')
+        div.innerHTML = `
+        <div class="card w-85 bg-base-100 shadow-xl">
+        <figure><img src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
+        <div class="card-body">
+          <h2 class="card-title">Shoes!</h2>
+          <p>If a dog chews shoes whose shoes does he choose?</p>
+          <div class="card-actions justify-end">
+            <button class="btn btn-primary">Buy Now</button>
+          </div>
+        </div>
+      </div>
+        `
+        productsContainer.appendChild(div)
+    })
 }
 
 displayMenu()
